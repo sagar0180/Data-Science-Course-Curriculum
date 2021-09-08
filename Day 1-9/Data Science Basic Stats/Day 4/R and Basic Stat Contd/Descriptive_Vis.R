c56ee0f5e7ce4995e4bbba34bb5bb148cffbd2ee
Sagar <75463858+sagar0180@users.noreply.github.com>
2021-09-08 11:50:18 +0530

    R, R Studio, Descriptive Stats in R

---
+++ Day 1-9/Data Science Basic Stats/Day 4/R and Basic Stat Contd/Descriptive_Vis.R
@@ -0,0 +1,79 @@
+#airquality = read.csv('path/airquality.csv',header=TRUE, sep=",")
+airquality <- datasets::airquality
+####Top 10 rows and last 10 rows
+head(airquality,10)
+tail(airquality,10)
+######Columns
+airquality[,c(1,2)]
+
+df<-airquality[,-6]
+
+summary(airquality[,1])
+
+airquality$Wind
+
+summary(airquality$Wind)
+
+###########Summary of the data#########
+
+summary(airquality)
+summary(airquality$Wind) 
+
+#####################
+plot(airquality$Wind)
+plot(airquality$Temp,airquality$Wind,type="p")
+plot(airquality)
+# points and lines 
+plot(airquality$Wind, type= "l") # p: points, l: lines,b: both
+plot(airquality$Wind, xlab = 'Ozone Concentration', 
+     ylab = 'No of Instances', main = 'Ozone levels in NY city',
+     col = 'blue')
+# Horizontal bar plot
+barplot(airquality$Ozone, main = 'Ozone Concenteration in air',
+        ylab = 'ozone levels', col= 'blue',horiz = F,axes=T)
+#Histogram
+hist(airquality$Temp)
+hist(airquality$Temp, 
+     main = 'Solar Radiation values in air',
+     xlab = 'Solar rad.', col='blue')
+
+#Single box plot
+boxplot(airquality$Wind,main="Boxplot")
+# Multiple box plots
+boxplot(airquality[,1:4],main='Multiple')
+#margin of the grid(mar), 
+#no of rows and columns(mfrow), 
+#whether a border is to be included(bty) 
+#and position of the 
+#labels(las: 1 for horizontal, las: 0 for vertical)
+#bty - box around the plot
+par(mfrow=c(3,3),mar=c(2,5,2,1),  las=0, bty="o")
+plot(airquality$Ozone)
+plot(airquality$Ozone, airquality$Wind)
+plot(airquality$Ozone, type= "l")
+plot(airquality$Ozone, type= "l")
+plot(airquality$Ozone, type= "l")
+barplot(airquality$Ozone, main = 'Ozone Concenteration in air',
+        xlab = 'ozone levels', col='green',horiz = TRUE)
+hist(airquality$Solar.R)
+boxplot(airquality$Solar.R)
+boxplot(airquality[,0:4], main='Multiple Box plots')
+
+
+##Home work
+e_quakes<-datasets::quakes
+sd(airquality$Ozone,na.rm = T)
+
+#var
+#skewness
+#kurtosis
+#Density plot
+
+
+
+
+
+
+
+
+
